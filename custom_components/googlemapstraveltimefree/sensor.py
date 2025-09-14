19183a694d0a0f7945c7a9a6328d4c08584df7f4
Renegaded66 <drostedevon@gmail.com>
2025-09-14 16:06:38 +0200

    Changed Sensor

--- custom_components/googlemapstraveltimefree/sensor.py
+++ custom_components/googlemapstraveltimefree/sensor.py
@@ -1,54 +1,74 @@
-"""Two Sensors, one for the travel time and one for last update"""
-
-from homeassistant.components.sensor import SensorDeviceClass, SensorEntity
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
-
-from .coordinator import TravelTimeCoordinator
+import logging
+from pathlib import Path
+import subprocess
+import sys
 
 
-class TravelTimeSensor(CoordinatorEntity, SensorEntity):
-    def __init__(self, coordinator: coordinator, entry_id: str, idx: int, title: str):
-        super().__init__(coordinator)
-        self._idx = idx
-        self._attr_name = f"{title} Travel Time"
-        self._attr_unique_id = f"{entry_id}_{idx}"
-        self._attr_native_unit_of_measurement = "min"
-        self._attr_should_poll = False
-
-    @property
-    def native_value(self):
-        return self.coordinator.data.get(self._idx) if self.coordinator.data else None
+_LOGGER = logging.getLogger(__name__)
 
 
-class TravelTimeLastUpdateSensor(CoordinatorEntity, SensorEntity):
-    def __init__(self, coordinator, entry_id: str, idx: int, title: str):
-        super().__init__(coordinator)
-        self._idx = idx
-        self._attr_name = f"{title} Last Update"
-        self._attr_unique_id = f"{entry_id}_{idx}_last_update"
-        self._attr_device_class = SensorDeviceClass.TIMESTAMP
-        self._attr_should_poll = False
-
-    @property
-    def native_value(self) -> str | None:
-        return self.coordinator.last_update
+import subprocess
+import sys
+from pathlib import Path
 
 
-async def async_setup_entry(
-    hass: HomeAssistant,
-    entry: ConfigEntry[TravelTimeCoordinator],
-    async_add_entities: AddEntitiesCallback,
-) -> None:
-    """Set up sensors from a config entry."""
-    coordinator = entry.runtime_data
-    title = entry.title
-    entities = []
-    for idx, _ in enumerate(coordinator.config.get("coordinates", [])):
-        entities.append(TravelTimeSensor(coordinator, entry.entry_id, idx, title))
-        entities.append(
-            TravelTimeLastUpdateSensor(coordinator, entry.entry_id, idx, title)
-        )
-    async_add_entities(entities)  # , update_before_add=True)
+def ensure_playwright_installed():
+    try:
+        subprocess.check_call(["playwright", "--version"], stderr=subprocess.DEVNULL)
+    except FileNotFoundError:
+        _LOGGER.error("Playwright CLI not found.")
+        return
+
+    try:
+        # Browser installieren, falls noch nicht installiert
+        subprocess.check_call(["playwright", "install", "firefox"])
+        _LOGGER.info("Successfully installed playwright browser.")
+    except Exception as e:
+        print(f"Exception: {e}")
+        _LOGGER.error(f"Error while installing browser: {e}")
+
+
+def berechne_reisezeit(start: str, end: str) -> float:
+    ensure_playwright_installed()
+    from playwright.sync_api import sync_playwright
+
+    _LOGGER.info(str(str(start) + "," + str(end)))
+    try:
+        with sync_playwright() as p:
+            browser = p.firefox.launch(headless=True, slow_mo=100)
+            page = browser.new_page()
+            page.goto(f"https://maps.google.com/maps/dir/{start}/{end}")
+
+            # Warten, bis das Cookie-Zustimmungsbanner sichtbar ist
+            page.wait_for_selector(
+                'xpath=//button[contains(., "Alle ablehnen") or contains(., "Reject all") or contains(., "Rechazar todo") or contains(., "Tout refuser")]'
+            )
+
+            # Klicken auf den ersten gefundenen Button
+            page.locator(
+                'xpath=//button[contains(., "Alle ablehnen") or contains(., "Reject all") or contains(., "Rechazar todo") or contains(., "Tout refuser")]'
+            ).first.click()
+
+            # Warten, bis die Seite vollständig geladen ist
+            page.wait_for_timeout(5000)
+            html = page.inner_html("#section-directions-trip-0")
+            # print(html)
+            # fahrtzeit = html.split('<div class="Fk3sm fontHeadlineSmall delay-light">')[
+            #    1
+            # ]
+            fahrtzeit = html.split("</div>")[0].split("Fk3sm")[1].split('">')[1]
+            # print(fahrtzeit)
+            fahrtzeit = fahrtzeit.split("</div>")[0]
+            # print(fahrtzeit)
+            if "&nbsp;h" in fahrtzeit:
+                fahrtzeit = int(fahrtzeit.split("&nbsp;h")[0].strip()) * 60 + int(
+                    fahrtzeit.split("&nbsp;h")[1].strip().split(" ")[0].strip()
+                )
+            else:
+                fahrtzeit = int(fahrtzeit.strip().split(" ")[0].strip())
+            # if fahrtzeit.isnmeric():
+            return int(fahrtzeit)
+    except Exception as e:
+        print(e)
+        # _LOGGER.info(f"Error while fetching Website Data: {e}")
+        return -1
